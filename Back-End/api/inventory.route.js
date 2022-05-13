2b3cbec261a0e199930ca7a5afc5f2ca431f52c0
MrNeedToKnow <andy_chen123@hotmail.com>
2022-05-12 22:06:17 -0400

    ss

--- Back-End/api/inventory.route.js
+++
@@ -1,9 +0,0 @@
-// route file, this is where we create different routes people can have access
-
-import express from 'express'
-
-const router = express.Router()
-
-router.route('/').get((req,res) => res.send('default inventroy route is working'))
-
-export default router
