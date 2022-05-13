58a1bd50cc39a9214c6a42e658ed712e038b6056
MrNeedToKnow <andy_chen123@hotmail.com>
2022-05-12 22:24:05 -0400

    ss

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,1 +1,21 @@
-console.log('dsad')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const app = express()
+
+
+
+
+
+
+
+app.get('/api/inventory', (req,res) => {
+    res.send('get inventory')
+})
+
+
+
+
+
+// define port and set listen
+const port = process.env.PORT  || 8000
+app.listen(port, () => console.log(`server started on port ${port}`))
