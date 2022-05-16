f3599a9c8b6209cb5aaac103d2a6b9629019c7a7
NDC <andy_chen123@hotmail.com>
2022-05-16 10:45:36 -0400

    ss

--- Back-End/controller/deletedItemController.js
+++ Back-End/controller/deletedItemController.js
@@ -18,11 +18,38 @@ const getDeletedItems = asyncHandler (async (req, res) => {
 
 
 //@desc Undelete deleted items (back to active inventory)
-//@route GET/api/deleteditems
+//@route DELETE /api/deleteditems
 //@access public
 const UndeleteDeletedItems = asyncHandler (async (req, res) => {
 
+    //Condition: IF ID IS NOT FOUND
+    //1. send status code 400
+    //2. throw error message
+    const deletedItem = await DeletedItems.findById(req.params.id)
+    if(!deletedItem){
+        res.status(400)
+        throw new Error('Item Not Found In Database for Deletion')
+    }
 
+    // Condition: Default
+    // 1. move undeleted item to inventroy collection
+    // 2. remove the item from the deleted collection in the database
+    // 3. return new list of deleted items so front end can rerender
+    const itemUndeleted = await Inventory.create({
+        Item_Name: req.body.Item_Name,
+        Quantity:req.body.Quantity,
+        Item_Description:req.body.Item_Description,
+    })
+
+    await deletedItem.remove()
+
+    const deletedItems = await DeletedItems.find()
+
+    res.status(200).json({
+        id: req.params.id,
+        newList:deletedItems,
+        itemUndeleted: itemUndeleted
+    })
 
 })
 
